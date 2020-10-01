e06dba175ba2d229e32c56ecb146d99781124da8
Jeremiah Holden <jeremiah@cardconnectcp.com>
2020-09-30 18:08:31 -0700

    added html from example, css from example

---
+++ gen.js
@@ -0,0 +1,6 @@
+alert ("You're password is expired. Please generate a new one using this handy tool.");
+
+// values =
+    // "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+prompt ()
---
+++ psuedo.md
@@ -0,0 +1,4 @@
+alert you need a new password "click to help generate new password"
+prompt for password requirements
+selector for password requirements
+prompt with password criteria
